bc5f4ee935dcbd3fb21fa38c52257dabb3b892cd
Yao Jing Quek <yao.jing.quek@intel.com>
2023-05-19 19:41:42 +0800

    Add simple lfsr simulation

--- lib/src/lfsr.dart
+++ lib/src/lfsr.dart
@@ -4,29 +4,42 @@ import 'package:rohd/rohd.dart';
 
 class LFSR extends Module {
   Logic get lsfrOut => output('lsfr_output');
-  LFSR(Logic clk, Logic reset) {
+  LFSR(Logic clk, Logic reset, {int bitsNum = 16}) {
     clk = addInput('clk', clk);
     reset = addInput('reset', reset);
 
-    final lfsrOut = addOutput('lfsr_output');
-
-    final shiftReg = Logic(name: 'shift_register', width: 3);
+    final lfsrOut = addOutput('lfsr_output', width: bitsNum);
+    final shiftReg = Logic(name: 'shift_register', width: bitsNum);
 
     Sequential(clk, [
       If(reset, then: [
-        shiftReg < Const(0),
+        shiftReg < 1,
       ], orElse: [
-        shiftReg < [shiftReg[2] ^ shiftReg[1], shiftReg.slice(1, 0)].swizzle()
+        shiftReg <
+            [
+              shiftReg.slice(bitsNum - 2, 0),
+              shiftReg[bitsNum - 1] ^ shiftReg[bitsNum - 2],
+            ].swizzle(),
       ])
     ]);
 
-    shiftReg <= lfsrOut;
+    lfsrOut <= shiftReg;
   }
 }
 
-void main(List<String> args) async {
+void main() async {
   final clk = SimpleClockGenerator(10).clk;
   final reset = Logic(name: 'reset');
 
-  final lsfr = LFSR(clk, reset);
+  final lsfr = LFSR(clk, reset, bitsNum: 3);
+
+  await lsfr.build();
+
+  reset.inject(1);
+
+  WaveDumper(lsfr, outputPath: 'lfsr.vcd');
+
+  Simulator.registerAction(25, () => reset.put(0));
+  Simulator.setMaxSimTime(100);
+  await Simulator.run();
 }
